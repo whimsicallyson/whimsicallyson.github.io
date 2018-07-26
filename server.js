95e77f91ea47b5cb4cc2ae8ac8e6f14086856bf6
Glitch (fish-coat) <none>
2018-07-26 14:22:50 +0000

    💰🐕 Checkpoint ./server.js:9/400

--- server.js
+++ server.js
@@ -32,37 +32,35 @@ app.get('/', function(request, response) {
   var promiseArray = [];
   var dest = './public/images';
 
-fs.readFile('./views/index.html', function(err, data) {
-  data = data.toString();
-  var htmlBrokenURLs = data.match(new RegExp(/src="\/web.*?"/, 'ig'))
-  if (htmlBrokenURLs != null) {
-    for (var i = 0; i < htmlBrokenURLs.length; i++) {
-      var justURL = htmlBrokenURLs[i].slice(5,-1); // slice removes src=" and "
-      promiseArray.push(imageDownloader.image({ url: 'https://web.archive.org'+justURL, dest})
-      .then(({filename, image}) => {
-        data = data.replace(justURL, filename); // this isn't quite right
-        console.log(data);
-        fs.writeFile('/views/index.html', data);
-      }))
+  fs.readFile('./views/index.html', function(err, data) {
+    data = data.toString();
+    var htmlBrokenURLs = data.match(new RegExp(/src="\/web.*?"/, 'ig'))
+    if (htmlBrokenURLs != null) {
+      for (var i = 0; i < htmlBrokenURLs.length; i++) {
+        var justURL = htmlBrokenURLs[i].slice(5,-1); // slice removes src=" and "
+        promiseArray.push(imageDownloader.image({ url: 'https://web.archive.org'+justURL, dest})
+        .then(({filename, image}) => {
+          data = data.replace(justURL, filename);
+          fs.writeFile('/views/index.html', data);
+        }))
+      }
     }
-  }
-});
+  });
 
-fs.readFile('./public/style.css', function(err, data) {
-  data = data.toString();
-  var cssBrokenURLs = data.match(new RegExp(/url\(\/web.*?\)/, 'ig')) 
-  if (cssBrokenURLs != null) {
-    for (var i = 0; i < cssBrokenURLs.length; i++) {
-      var justURL = cssBrokenURLs[i].slice(4,-1); // slice removes url( and )
-      promiseArray.push(imageDownloader.image({ url: 'https://web.archive.org'+justURL, dest})
-      .then(({filename, image}) => {
-        data = data.replace(justURL, filename);
-        console.log(data);
-        fs.writeFile('./public/style.css', data);
-      }))
+  fs.readFile('./public/style.css', function(err, data) {
+    data = data.toString();
+    var cssBrokenURLs = data.match(new RegExp(/url\(\/web.*?\)/, 'ig')) 
+    if (cssBrokenURLs != null) {
+      for (var i = 0; i < cssBrokenURLs.length; i++) {
+        var justURL = cssBrokenURLs[i].slice(4,-1); // slice removes url( and )
+        promiseArray.push(imageDownloader.image({ url: 'https://web.archive.org'+justURL, dest})
+        .then(({filename, image}) => {
+          data = data.replace(justURL, filename);
+          fs.writeFile('./public/style.css', data);
+        }))
+      }
     }
-  }
-});
+  });
 
  // haven't touched this part yet 
   // when all the promises in that for/each are done
