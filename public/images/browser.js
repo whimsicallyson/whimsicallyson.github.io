36659f7c356b9716cc918db00f988244bcdc938c
Glitch (fish-coat) <none>
2018-07-26 19:24:12 +0000

    🍠🙉 Checkpoint ./views/index.html:9/26992 ./public/style.css:9/34136 ./server.js:9/4

---
+++ public/images/browser.js
@@ -0,0 +1,171 @@
+﻿$(document).ready(function () {
+
+  var BrowserDetect = {
+    init: function () {
+      this.browser = this.searchString(this.dataBrowser) || "unknown-browser";
+      this.version = this.searchVersion(navigator.userAgent)
+        || this.searchVersion(navigator.appVersion)
+        || "unknown-browser-version";
+      this.OS = this.searchString(this.dataOS) || "unknown-os";
+    },
+    searchString: function (data) {
+      for (var i = 0; i < data.length; i++) {
+        var dataString = data[i].string;
+        var dataProp = data[i].prop;
+        this.versionSearchString = data[i].versionSearch || data[i].identity;
+        if (dataString) {
+          if (dataString.indexOf(data[i].subString) != -1)
+            return data[i].identity;
+        }
+        else if (dataProp)
+          return data[i].identity;
+      }
+    },
+    searchVersion: function (dataString) {
+      var index = dataString.indexOf(this.versionSearchString);
+      if (index == -1) return;
+      return parseFloat(dataString.substring(index + this.versionSearchString.length + 1));
+    },
+    dataBrowser: [
+      {
+        string: navigator.userAgent,
+        subString: "Chrome",
+        identity: "Chrome"
+      },
+      {
+        string: navigator.userAgent,
+        subString: "OmniWeb",
+        versionSearch: "OmniWeb/",
+        identity: "OmniWeb"
+      },
+      {
+        string: navigator.vendor,
+        subString: "Apple",
+        identity: "Safari",
+        versionSearch: "Version"
+      },
+      {
+        prop: window.opera,
+        identity: "Opera"
+      },
+      {
+        string: navigator.vendor,
+        subString: "iCab",
+        identity: "iCab"
+      },
+      {
+        string: navigator.vendor,
+        subString: "KDE",
+        identity: "Konqueror"
+      },
+      {
+        string: navigator.userAgent,
+        subString: "Firefox",
+        identity: "Firefox"
+      },
+      {
+        string: navigator.vendor,
+        subString: "Camino",
+        identity: "Camino"
+      },
+      {   // for newer Netscapes (6+)
+        string: navigator.userAgent,
+        subString: "Netscape",
+        identity: "Netscape"
+      },
+      {
+        string: navigator.userAgent,
+        subString: "MSIE",
+        identity: "Explorer",
+        versionSearch: "MSIE"
+      },
+      {
+        string: navigator.userAgent,
+        subString: "Gecko",
+        identity: "Mozilla",
+        versionSearch: "rv"
+      },
+      {     // for older Netscapes (4-)
+        string: navigator.userAgent,
+        subString: "Mozilla",
+        identity: "Netscape",
+        versionSearch: "Mozilla"
+      }
+    ],
+    dataOS: [
+      {
+        string: navigator.platform,
+        subString: "Win",
+        identity: "Windows"
+      },
+      {
+        string: navigator.platform,
+        subString: "Mac",
+        identity: "Mac"
+      },
+      {
+        string: navigator.userAgent,
+        subString: "iPhone",
+        identity: "iPhone"
+      },
+      {
+        string: navigator.userAgent,
+        subString: "iPod",
+        identity: "iPod"
+      },
+      {
+        string: navigator.userAgent,
+        subString: "iPad",
+        identity: "iPad"
+      },
+      {
+        string: navigator.userAgent,
+        subString: "Andriod",
+        identity: "Andriod"
+      },
+      {
+        string: navigator.platform,
+        subString: "Linux",
+        identity: "Linux"
+      }
+    ]
+
+  };
+  BrowserDetect.init();
+
+  var browser_str = '' + BrowserDetect.browser + '';
+  var browser_str_lower = browser_str.toLowerCase();
+
+  var browser_ver_str = '' + BrowserDetect.browser + '-' + BrowserDetect.version + '';
+  var browser_ver_str_lower = browser_ver_str.toLowerCase();
+
+  var os_str = '' + BrowserDetect.OS + '';
+  var os_str_lower = os_str.toLowerCase();
+
+  $('body').addClass(browser_str_lower);
+  $('body').addClass(browser_ver_str_lower);
+  $('body').addClass(os_str_lower);
+
+});
+
+/*
+     FILE ARCHIVED ON 04:00:11 Aug 14, 2017 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 19:16:00 Jul 26, 2018.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  LoadShardBlock: 103.198 (3)
+  esindex: 0.008
+  captures_list: 127.284
+  CDXLines.iter: 12.836 (3)
+  PetaboxLoader3.datanode: 106.52 (4)
+  exclusion.robots: 0.87
+  exclusion.robots.policy: 0.86
+  RedisCDXSource: 5.425
+  PetaboxLoader3.resolve: 205.479
+  load_resource: 223.228
+*/
---
+++ public/images/tracking.js
@@ -0,0 +1,38 @@
+﻿
+//Adroll
+    adroll_adv_id = "QMA3A4BZ2ZBZNAK3HW55FB";
+    adroll_pix_id = "ZOH6FRT5KNE77PL33PNCA2";
+    (function () {
+        var oldonload = window.onload;
+        window.onload = function () {
+            __adroll_loaded = true;
+            var scr = document.createElement("script");
+            var host = (("https:" == document.location.protocol) ? "https://web.archive.org/web/20170814040029/https://s.adroll.com/" : "https://web.archive.org/web/20170814040029/http://a.adroll.com/");
+            scr.setAttribute('async', 'true');
+            scr.type = "text/javascript";
+            scr.src = host + "/j/roundtrip.js";
+            document.documentElement.firstChild.appendChild(scr);
+            if (oldonload) { oldonload() } 
+        };
+    } ());
+/*
+     FILE ARCHIVED ON 04:00:29 Aug 14, 2017 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 19:15:59 Jul 26, 2018.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  LoadShardBlock: 75.521 (3)
+  esindex: 0.012
+  captures_list: 109.22
+  CDXLines.iter: 14.717 (3)
+  PetaboxLoader3.datanode: 60.2 (4)
+  exclusion.robots: 0.128
+  exclusion.robots.policy: 0.119
+  RedisCDXSource: 15.195
+  PetaboxLoader3.resolve: 97.72
+  load_resource: 124.257
+*/
