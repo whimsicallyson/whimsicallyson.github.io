a76b6bab987d1fc3509b0d82c1ec270f2fb273f2
Glitch (whimsicallyson) <none>
2018-10-30 19:43:07 +0000

    🏪👊 Checkpoint ./README.md:9/1820 ./server.js:9/2470 ./.glitch-assets:9/408 ./views/index.html:9/350 ./public/style.css:9/17

--- README.md
+++ README.md
@@ -1,28 +1,2 @@
-HELP US BRING BACK THE CREATIVE WEB!
-====================================
-Modern websites all look the same. And for some, the internet has literally just become Facebook. Compared to the early days of the web, where millions of people around the world were creating weird and wonderful websites that expressed themselves, the modern web can feel soulless.
-
-But we know the good, fun internet is still out there. Help us bring back the creative web and show everyone how it's still a thing we all make, not just something we consume.
-
-Show us how you got started building the web. Bring back your first web projects and share them with the world.
-
-GET STARTED
------------
-- Find your first website on [archive.org](https://archive.org/)
-- Paste the URL into the [Wayback importer app](http://wayback-importer.glitch.me/scraper).
-
-The imported will extract the code for your site from the archive.
-
-- Then hit "click here to make your site live again" and a new Glitch project will open in a new tab.
-- Copy the HTML from the Wayback importer and paste it into your new Glitch project in the file `views/index.html` between the comments: "START website importer HTML code" and "END website importer HTML code".
-- Do the same for your CSS. Copy the CSS from the Wayback importer and paste it into `public/style.css.`
-Your app will now automatically look through your code for images, and save them in your project. Note: If your site has a ton of images then they might not all download automatically
-
--Once you see the green 'Live' button, hit "show" to see your website live on Glitch.
-
-SHARE YOUR SITE
----------------
-When you're happy with it, share it online for all to see and [submit it to us](https://airtable.com/shrwcr9CJ4Oytt0Z0) to be included in our [collection of first sites](https://glitch.com/websites).
-
 
 \ ゜o゜)ノ
--- server.js
+++ server.js
@@ -4,12 +4,6 @@
 // init project
 var express = require('express');
 var app = express();
-var bodyParser = require('body-parser');
-var imageDownloader = require('image-downloader');
-var fs = require('fs');
-
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
 
 // we've started you off with Express, 
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
@@ -21,76 +15,6 @@ app.use(express.static('public'));
 app.get('/', function(request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
-
-
-// for each node restart
-// 1. get index.html and style.css
-// 2. parse code for broken image links
-// 3. for each image link, download
-
-  
-var promiseArray = [];
-var dest = './public/images';
-
-fs.readFile('./views/index.html', function(err, data) {
-  var indexHTML = data.toString();
-  var htmlBrokenURLs = indexHTML.match(new RegExp(/src="\/web.*?"/, 'ig'));
-
-  if (htmlBrokenURLs !== null) {
-    for (var i = 0; i < htmlBrokenURLs.length; i++) {
-      var justURL = htmlBrokenURLs[i].slice(5,-1); // slice removes src=" and "
-      var filename = '/images/' + justURL.split('/').pop();
-      indexHTML = indexHTML.replace(justURL, filename);
-      promiseArray.push(
-        imageDownloader.image({ url: 'https://web.archive.org'+justURL, dest})
-          .catch((err) => { console.log(justURL, 'imagedownload failed', err); })
-      )
-    }
-
-    // when all the promises in that for/each are done
-    Promise.all( promiseArray )
-      .then( () => {
-        var newHTML = indexHTML;
-        fs.writeFile('./views/index.html', newHTML, (err) => {
-          console.log('html file written. error? ', err); 
-        }); 
-      })
-      .catch((err) => {
-        console.error("HTML image error", err);
-        console.log(promiseArray);
-      });
-  }
-});
-
-
-fs.readFile('./public/style.css', function(err, data) {
-  var indexCSS = data.toString();
-  var cssBrokenURLs = indexCSS.match(new RegExp(/url\(\/web.*?\)/, 'ig')) 
-
-  if (cssBrokenURLs !== null) {
-    for (var i = 0; i < cssBrokenURLs.length; i++) {
-      var justURL = cssBrokenURLs[i].slice(4,-1); // slice removes url( and )
-      console.log(justURL);
-      var filename = '/images/' + justURL.split('/').pop();
-      indexCSS = indexCSS.replace(justURL, filename);
-      promiseArray.push(imageDownloader.image({ url: 'https://web.archive.org'+justURL, dest})
-        .catch((err) => { console.log(justURL, 'imagedownload failed', err); })    
-      )
-    }
-    Promise.all( promiseArray )
-      .then( () => {
-        var newCSS = indexCSS;
-        fs.writeFile('./public/style.css', newCSS, (err) => {
-          console.log('css file written. error? ', err); 
-        }); 
-      })
-      .catch((err) => {
-        console.error("CSS image error", err);
-        console.log(promiseArray);
-      });
-  }
-});
-
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function() {
