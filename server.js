8ae3d68a3290778ace4d369d833cb34531611825
Glitch (whimsicallyson) <none>
2019-08-14 02:07:42 +0000

    🍎🗻 Checkpoint ./.glitch-assets:9/400 ./server.js:9/1172

--- server.js
+++ server.js
@@ -4,6 +4,7 @@
 // init project
 var express = require('express');
 var app = express();
+var fs = require('fs');
 
 // we've started you off with Express, 
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
@@ -15,6 +16,14 @@ app.use(express.static('public'));
 app.get('/', function(request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
+
+app.get('/resume', function(request, response) {
+  var data = response.sendFile('https://cdn.glitch.com/32451e03-57c0-4098-8fc0-2c75d9c7dea0%2FAllyson%20Lubimir%20Resume.pdf?v=1565748016876');
+  response.contentType("application/pdf");
+  response.send(data);
+});
+
+
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function() {
