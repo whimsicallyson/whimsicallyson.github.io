97164101d014744d2acdfeeeaa77fc55826f7647
Glitch (whimsicallyson) <none>
2019-08-14 02:18:49 +0000

    👩‍❤️‍💋‍👩😬 Checkpoint ./.glitch-assets:9/886 ./server.js:9/268

--- server.js
+++ server.js
@@ -17,14 +17,6 @@ app.get('/', function(request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
 
-app.get('/resume', function(request, response) {
-  var data = response.sendFile('https://cdn.glitch.com/32451e03-57c0-4098-8fc0-2c75d9c7dea0%2FAllyson%20Lubimir%20Resume.pdf?v=1565748016876');
-  response.contentType("application/pdf");
-  response.send(data);
-});
-
-
-
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function() {
   console.log('Your app is listening on port ' + listener.address().port);
